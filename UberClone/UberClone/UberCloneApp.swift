14e83b18f5381ff25eae016be0e8293525d23547
mustaphazeroual04 <mustaphazeroual01@gmail.com>
2023-11-27 19:17:14 +0100

    init uberClone project

---
+++ UberClone/UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by MUSTAPHA on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
