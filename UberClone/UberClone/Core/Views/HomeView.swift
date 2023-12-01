bc81812dfbcfaf6a026d89b51cd55859d8b3edb7
mustaphazeroual04 <mustaphazeroual01@gmail.com>
2023-12-01 09:37:34 +0100

    refactor(app-structure): :sparkles: init new App structure

---
+++ UberClone/UberClone/Core/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  UberClone
+//
+//  Created by MUSTAPHA on 1/12/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        UberMapViewRepresentable()
+            .ignoresSafeArea()
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ UberClone/UberClone/Core/Views/UberMapViewRepresentable.swift
@@ -0,0 +1,44 @@
+//
+//  UberMapViewRepresentable.swift
+//  UberClone
+//
+//  Created by MUSTAPHA on 1/12/2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct UberMapViewRepresentable: UIViewRepresentable {
+    
+    
+    let mapView = MKMapView()
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+
+extension UberMapViewRepresentable {
+    class MapCoordinator : NSObject, MKMapViewDelegate {
+        let parent: UberMapViewRepresentable
+        
+        
+        init(parent: UberMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+    }
+}
